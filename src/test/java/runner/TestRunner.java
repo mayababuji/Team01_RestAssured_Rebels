88e3987e8c07eeb407d566c235dcea0bd66c7078
Maya Babuji <mayababuji@outlook.com>
2026-09-17 12:17:36 -0400

    added extent report

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,9 +8,13 @@ import io.cucumber.testng.CucumberOptions;
         glue = {"stepDefinitions","hooks"},
         plugin = {
                 "pretty",
-                "html:target/cucumber-report.html",
-                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
-        }
+                "html:target/cucumber-reports/cucumber.html",
+                "json:target/cucumber-reports/cucumber.json",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        },
+        monochrome = false,
+        publish = false
 )
 public class TestRunner extends AbstractTestNGCucumberTests{
 
